417bd7d54a763dad5a202ddd19d894c29cde7388
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-07-09 05:38:41 +0600

    Add FTP connection details and paths of the directory

---
+++ AutomatedFileProcessor.py
@@ -0,0 +1,13 @@
+import ftplib
+import os
+
+# ftp info
+FTP_HOST = "localhost"
+FTP_USER = "nybsys"
+FTP_PASS = "12345"
+FTP_PORT = 21
+
+# paths of directory
+TEMP_DIR = "temp"
+LOCAL_DIR = "local"
+TRASH_DIR = "trash"
