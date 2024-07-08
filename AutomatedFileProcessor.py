09eec2a793277829624bf88f0ac05586aebdd0b1
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-07-09 05:47:54 +0600

    Implement download_files function for downlaod and store files from FTP server

--- AutomatedFileProcessor.py
+++ AutomatedFileProcessor.py
@@ -11,3 +11,22 @@ FTP_PORT = 21
 TEMP_DIR = "temp"
 LOCAL_DIR = "local"
 TRASH_DIR = "trash"
+
+
+# Function for download files from FTP Server
+def download_files():
+    ftp = ftplib.FTP()
+    ftp.connect(FTP_HOST, FTP_PORT)
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.cwd('/')
+
+    filenames = ftp.nlst()
+
+    for filename in filenames:
+        local_temp_path = os.path.join(TEMP_DIR, filename)
+        with open(local_temp_path, 'wb') as f:
+            ftp.retrbinary('RETR ' + filename, f.write)
+
+            local_final_path = os.path.join(LOCAL_DIR, filename)
+            os.rename(local_temp_path, local_final_path)
+    ftp.quit()
