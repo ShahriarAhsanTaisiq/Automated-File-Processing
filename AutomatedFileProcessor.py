5589decf78d20328b509c0a463e9ab7782c5c1ec
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-07-13 10:07:04 +0600

    Add function to download XML files from FTP server

--- AutomatedFileProcessor.py
+++ AutomatedFileProcessor.py
@@ -1,3 +1,4 @@
+import ftplib
 import os
 import logging
 
@@ -13,3 +14,30 @@ FTP_PORT = int(os.getenv("FTP_PORT", 21))
 TEMP_DIR = "temp"
 LOCAL_DIR = "local"
 TRASH_DIR = "trash"
+
+
+# function for download files
+def download_files():
+    try:
+        ftp = ftplib.FTP()
+        ftp.connect(FTP_HOST, FTP_PORT)
+        ftp.login(FTP_USER, FTP_PASS)
+        ftp.cwd('/')
+
+        filenames = ftp.nlst('*.xml')
+        for filename in filenames:
+            local_temp_path = os.path.join(TEMP_DIR, filename)
+            if not os.path.exists(local_temp_path):
+                with open(local_temp_path, 'wb') as f:
+                    ftp.retrbinary('RETR ' + filename, f.write)
+
+                local_final_path = os.path.join(LOCAL_DIR, filename)
+                os.rename(local_temp_path, local_final_path)
+                logging.info(f"Downloaded {filename} to {LOCAL_DIR}")
+
+        ftp.quit()
+
+    except Exception as e:
+        logging.error(f"Error during file download or processing: {str(e)}")
+        if 'ftp' in locals():
+            ftp.quit()
